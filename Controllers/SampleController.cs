0504e4ddf5df423068021a195994ee58577fda03
vivek <vivek@DESKTOP-L3TH11L>
2024-02-14 13:13:39 +0530

    Add project files.

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZeissMVCDemo.Controllers
+{
+    public class SampleController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Demo(int id)
+        {
+            Console.WriteLine($"Code is: {id}");
+            return View();
+        }
+
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace ZeissMVCDemo.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+    }
+}
